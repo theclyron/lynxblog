d79464fd51fdbfdf06931ce65bb5eb8b45cdf21e
Lynxmic <30957824+Lynxmic@users.noreply.github.com>
2022-12-30 13:51:30 +0200

    separate navbar script from main html files

---
+++ docs/js/navbar.js
@@ -0,0 +1,8 @@
+$(function() {
+$(document).ready(function() {
+    $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+});
+});
+});
