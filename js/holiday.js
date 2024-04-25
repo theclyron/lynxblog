5a643ad6e49b0a8589d8c93fde34aade946fecd5
Lynx <30957824+Lynxmic@users.noreply.github.com>
2024-04-25 19:24:57 +0300

    a

--- js/holiday.js
+++ js/holiday.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const currentMonth = currentDate.getMonth() + 1;
   
     if (currentMonth === 12 && currentDate.getDate() >= 25) {
-      showSnowflakes(); // Call function to show snowflakes
+      showSnowflakes();
     } else if (currentMonth === 1 && currentDate.getDate() === 1) {
-      showSnowflakes(); // Call function to show snowflakes
+      showSnowflakes();
     }
   
     function showSnowflakes() {
       const snowflakesElement = document.getElementById('snowflakes');
-      snowflakesElement.style.display = 'block'; // Show the snowflakes
+      snowflakesElement.style.display = 'block';
     }
   });
   
